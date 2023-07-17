83526df09a629df14c369e9781322a297167b5b7
tranphong1996 <0973513040phong@gmail.com>
2023-07-17 15:29:44 +0700

    lan thu 2 lam chuyen ay

--- appClientModule/com/fu/mathutil/MathUtilLive.java
+++ appClientModule/com/fu/mathutil/MathUtilLive.java
@@ -8,6 +8,14 @@ public class MathUtilLive {
 		//expected value: 5! = 120
 		//mày/hàm ai đó viết khi chạy thực tế là mấy -actual, giả sử 120
 		//if expected == actual, hàm ngon trong tình huống này
+		
+		long result = MathUtil.getFactorial(5);
+		System.out.println("5! = 120: actual:  "+ result);
+		//kĩ thuật kiểm thử phần mềm: ước lượng xem giá trị trả về của hàm là gì
+		//expected value: 5! = 120
+		//mày/hàm ai đó viết khi chạy thực tế là mấy -actual, giả sử 120
+		//if expected == actual, hàm ngon trong tình huống này
+		
 	}
 
 }
